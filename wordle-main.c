830f5204eac1ff59c1f906be41b5d0ba41258e84
Andre <lungua0@gmail.com>
2024-09-21 21:47:42 -0400

    Update wordle-main.c

--- wordle-main.c
+++ wordle-main.c
@@ -1,4 +1,3 @@
-/* hw3-main.c */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +8,6 @@ int total_wins;
 int total_losses;
 char ** words;
 
-/* write the wordle_server() function and place all of your code in hw3.c */
 int wordle_server( int argc, char ** argv );
 
 int main( int argc, char ** argv )
@@ -20,9 +18,6 @@ int main( int argc, char ** argv )
 
   int rc = wordle_server( argc, argv );
 
-  /* on Submitty, there will be more code here that validates
-   *  the global variables at the end of your code...
-   */
 
   /* deallocate memory for the list of words played */
   for ( char ** ptr = words ; *ptr ; ptr++ )
